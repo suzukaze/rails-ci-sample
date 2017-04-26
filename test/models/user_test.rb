49c0536a2b65613edcdfda33e32cb664e7d50ad6
Jun Hiroe <Jun.Hiroe@gmail.com>
2017-04-26 18:41:30 +0900

    Userモデルにテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(username: 'takashi777', nickname: 'hoge', email: 'takashi@example.com')
+  end
+
+  test 'Userオブジェクトが正しい' do
+    assert @user.valid?
+  end
+
+  test 'usernameが正しい' do
+    assert @user.valid?
+
+    @user.username = 'takashi?'
+    assert_not @user.valid?
+  end
 end
