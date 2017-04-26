d514c7aa03ff7c2d893a07de94a5f2efb8853471
Jun Hiroe <Jun.Hiroe@gmail.com>
2017-04-26 18:41:13 +0900

    Userモデルにvalidateを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+  VALID_USERNAME_REGEX = /\A[a-z0-9_]+\z/i
+  validates :username, presence: true, length: { in: 2..32 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: true
 end
