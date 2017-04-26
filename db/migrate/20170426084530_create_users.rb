5abcafe26e84e94babbc27b3420111d1fc6230c5
Jun Hiroe <Jun.Hiroe@gmail.com>
2017-04-26 17:52:45 +0900

    Userモデルを追加

---
+++ db/migrate/20170426084530_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username, index: true, unique: true
+      t.string :nickname
+      t.string :email
+      t.timestamps null: false
+    end
+  end
+end
